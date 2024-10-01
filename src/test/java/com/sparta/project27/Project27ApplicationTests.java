2a80e65c327b822405ac1cfe9bc4a5149666bdc7
KTJ-1650 <xowns1650@gmail.com>
2024-10-01 09:56:07 +0900

    Let's play init

---
+++ src/test/java/com/sparta/project27/Project27ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.project27;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project27ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
